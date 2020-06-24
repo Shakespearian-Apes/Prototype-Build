910210eea58221a3930898114e3f57138d851d2c
otwald <hinterlanddev@googlemail.com>
2020-06-24 23:34:30 +0200

    trying to repair the wrong merge

--- fix/Enemies/StateMachine/Idle.gd
+++
@@ -1,8 +0,0 @@
-extends "EnemyState.gd"
-
-
-func update_process(machine, delta):
-	if parent.noplayer:
-		machine._change_state("playerdead")
-	if this_enemy.see_player:
-		machine._change_state("walk")
--- fix/Enemies/StateMachine/PlayerDead.gd
+++
@@ -1,5 +0,0 @@
-extends "EnemyState.gd"
-
-func update_process(machine, delta):
-	if not parent.noplayer:
-		machine._change_state("idle")
