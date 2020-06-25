45e10b300f22449044601979980a857776498b73
otwald <hinterlanddev@googlemail.com>
2020-06-25 15:59:59 +0200

    added new icos to hud szenes

--- Enemies/Speedster/Speed_Enemy.gd
+++ Enemies/Speedster/Speed_Enemy.gd
@@ -20,22 +20,22 @@ func _ready():
 	sword.connect("AttackFinished", self, "reset_attack")
 
 #checks if body that enters Detection is the Player
-func detection_entered(body : Node):
+func detection_entered(body : Node)->void:
 	if body == root.player_node:
 		see_player = true
 
 #checks if the body that is leavin the Area is the Player
-func detection_exited(body : Node):
+func detection_exited(body : Node)->void:
 	if see_player and (body == root.player_node):
 		see_player = false
 
-func attack():
+func attack()->void:
 	if not attack_ready:
 		return
 	sword._change_state("attack")
 	# $AnimationPlayer.play("attack")
 
-func reset_attack():
+func reset_attack()->void:
 	attack_ready = true;
 
 # relays the Damage handling to the StateMachine
